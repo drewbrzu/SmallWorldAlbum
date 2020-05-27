69b4942ee6e7787b700c82011bfff817c3a180ab
drewbrzu <drewbrzu@gmail.com>
2020-05-26 20:52:19 -0400

    Allow user to update profile information

--- app_package/forms.py
+++ app_package/forms.py
@@ -1,4 +1,6 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from app_package.models import User
@@ -39,3 +41,31 @@ class LoginForm(FlaskForm):
         validators=[DataRequired()])
     remember = BooleanField("Remember Me")
     submit = SubmitField("Login")
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField("Username", 
+        validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField("Email", 
+        validators=[DataRequired(), Email()])
+    first_name = StringField("First Name", 
+        validators=[DataRequired()])
+    last_name = StringField("Last Name", 
+        validators=[DataRequired()])
+
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg','png'])])
+    submit = SubmitField("Update")
+
+    def validate_username(self, username):
+        """ This method is a custom form validation that checks if username already exist in our database. """
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is already taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        """ This method is a custom form validation that checks if email already exist in our database. """
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email is already associated with an account. Please click forgot username/password if you forgot your username.')
--- app_package/routes.py
+++ app_package/routes.py
@@ -4,9 +4,12 @@
 
 from flask import render_template, url_for, flash, redirect, request
 from app_package import app, db, bcrypt
-from app_package.forms import RegistrationForm, LoginForm
+from app_package.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from app_package.models import User
 from flask_login import login_user, current_user, logout_user, login_required
+# import secrets
+# import os
+# from PIL import Image
 
 @app.route("/")
 @app.route("/home")
@@ -46,10 +49,39 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
-@app.route("/user_preferences", methods=["GET"])
+# def save_picture(form_picture):
+#     random_hex = secrets.token_hex(8) # Create a random 8 digit hexadecial to use as the file name
+#     f_name, f_ext = os.path.splitext(form_picture.filename) # This os function returns 2 parameters: the filename without extension, and the filename extension
+#     picture_fn = random_hex + f_ext
+#     picture_path = os.path.join(app.root_path, 'static/images', picture_fn) # This creates the full file path to the image.
+
+#     output_size = (125, 125) # x/y size of picture
+#     i = Image.open(form_picture) # use the PIL library to open image object
+#     i.thumbnail(output_size) # resize the image.
+#     i.save(picture_path) # This saves the file to the newly created path.
+#     return picture_fn # return the new hexadecial filename we created.
+
+
+@app.route("/user_preferences", methods=["GET", "POST"])
 @login_required
 def user_preferences():
-    return render_template('user_preferences.html', title='User Preferences')
+    form = UpdateAccountForm()
+    if request.method == 'POST' and form.validate():
+        # if form.picture.data:
+        #     picture_name = save_picture(form.picture.data)
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        current_user.firstname = form.first_name.data
+        current_user.lastname = form.last_name.data
+        db.session.commit()
+        flash('Your account information has been updated!', 'success')
+        return redirect(url_for('user_preferences'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+        form.first_name.data = current_user.firstname
+        form.last_name.data = current_user.lastname
+    return render_template('user_preferences.html', form=form)
 
 
 """
@@ -60,147 +92,3 @@ def generic(pathname):
     return f"Hello, {pathname}"
  """
 
-
-# @app.route("/login", methods=["GET", "POST"])
-# def login():
-#     """Log user in"""
-
-#     # Forget any user_id
-#     session.clear()
-
-#     # User reached route via POST (as by submitting a form via POST)
-#     if request.method == "POST":
-
-#         # Ensure username was submitted
-#         if not request.form.get("username"):
-#             return redirect("/") #error("must provide username", 403)
-
-#         # Ensure password was submitted
-#         elif not request.form.get("password"):
-#             return redirect("/") #error("must provide password", 403)
-
-#         # Query database for username
-#         rows = db.execute(
-#             "SELECT * FROM users WHERE username = :username",
-#             username=request.form.get("username"),
-#         ).fetchall()
-#         # with sqlite3.connect("mydatabase.db") as con:
-#         #     cur = con.cursor()
-#         #     cur.execute(
-#         #         "SELECT * FROM users WHERE username = ?", request.form.get("username")
-#         #     )
-#         #     rows = cur.fetchall()
-#         #     con.commit()
-#         # Query database for username
-#         # rows = db.execute(
-#         #     "SELECT * FROM users WHERE username = :username",
-#         #     username=request.form.get("username"),
-#         # )
-
-#         # Ensure username exists and password is correct
-#         if len(rows) != 1 or not check_password_hash(
-#             rows[0]["hash"], request.form.get("password")
-#         ):
-#             return redirect("/") #error("invalid username and/or password", 403)
-
-#         # Remember which user has logged in
-#         session["user_id"] = rows[0]["id"]
-
-#         # Redirect user to home page
-#         return redirect("/")
-
-#     # User reached route via GET (as by clicking a link or via redirect)
-#     # Display the page for user to enter their info to log in.
-#     else:
-#         return render_template("login.html")
-
-
-# @app.route("/forgotpassword", methods=["GET", "POST"])
-# def forgotpassword():
-#     if request.method == "POST":
-#         if not request.form.get("email"):
-#             return redirect("/")  # error("please enter an email address")
-#         return redirect("/")
-#     else:
-#         return render_template("user_account/forgotpassword.html")
-
-
-# @app.route("/register", methods=["GET", "POST"])
-# def register():
-#     if request.method == "POST":
-#         # Grab form data
-#         username = request.form.get("username")
-#         password = request.form.get("password")
-#         password2 = request.form.get("password2")
-#         firstname = request.form.get("firstname")
-#         lastname = request.form.get("lastname")
-#         email = request.form.get("email")
-
-#         # Check form inputs
-#         if (
-#             not username
-#             or not password
-#             or not password2
-#             or not firstname
-#             or not lastname
-#             or not email
-#         ):
-#             return redirect("/") #error("please enter all information.")
-
-#         if not password == password2:
-#             return redirect("/") #error("passwords must match.")
-
-#         if not "@" in email:
-#             return redirect("/") #error("please enter a valid email.")
-
-#         queryRes = db.execute(
-#             "SELECT username FROM users WHERE username = :username", username=username
-#         ).fetchall()
-
-#         # Start database
-#         # with sqlite3.connect("mydatabase.db") as con:
-#         #     cur = con.cursor()
-#         #     cur.execute("SELECT username FROM users WHERE username = ?", (username))
-#         #     queryRes = cur.fetchall()
-#         #     con.commit()
-
-#         if len(queryRes) < 1:
-#             db.execute(
-#                 "INSERT INTO users (username, hash, firstname, lastname, email) VALUES (:username, :hash, :fname, :lname, :email)",
-#                 username=username,
-#                 hash=generate_password_hash(password),
-#                 fname=firstname,
-#                 lname=lastname,
-#                 email=email,
-#             )
-#             # with sqlite3.connect("mydatabase.db") as con:
-#             #     cur = con.cursor()
-#             #     cur.execute(
-#             #         "INSERT INTO users (username, hash, firstname, lastname, email) VALUES (?, ?, ?, ?, ?)",
-#             #         (
-#             #             username,
-#             #             generate_password_hash(password),
-#             #             firstname,
-#             #             lastname,
-#             #             email,
-#             #         ),
-#             #     )
-#             #     con.commit()
-#             redirect("/login")
-#         else:
-#             error("Sorry, that username is already taken.")
-#         return redirect("/")
-
-#     else:
-#         return render_template("user_account/register.html")
-
-
-# @app.route("/logout")
-# def logout():
-#     """Log user out"""
-
-#     # Forget any user_id
-#     session.clear()
-
-#     # Redirect user to login form
-#     return redirect("/")
